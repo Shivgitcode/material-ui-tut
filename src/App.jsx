bc841320e46ee8ba18c2c8d86f47568a85019058
shivansh <shivneeraj2004@gmail.com>
2024-03-18 18:25:43 +0530

    done with custome themes and text fields and radio form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fb8500",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+  },
+});
 
 function App() {
   return (
     <div>
-      <Create></Create>
-      <Notes></Notes>
+      <ThemeProvider theme={theme}>
+        <Create></Create>
+        <Notes></Notes>
+      </ThemeProvider>
     </div>
   );
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import makeStyles from "@mui/styles/makeStyles";
+import { TextField } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useState } from "react";
+import { Radio, RadioGroup, FormControlLabel } from "@mui/material";
+import { useEffect } from "react";
 
 const useStyles = {
   btn: {
@@ -13,29 +16,100 @@ const useStyles = {
       backgroundColor: "blue",
     },
   },
+  field: {
+    marginTop: 2,
+    marginBottom: 2,
+    display: "block",
+  },
+  radio: {
+    "&:hover": {
+      color: "secondary",
+    },
+  },
 };
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  function handleTitle(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleDesc(e) {
+    setDesc(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title === "" && desc === "") {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+  };
+
   return (
     <Container>
       <Typography
         variant="h6"
         component="h2"
         color="textSecondary"
+        fontWeight="bold"
         gutterBottom
       >
         Create a New Note
       </Typography>
 
-      <Button
-        sx={useStyles.btn}
-        onClick={() => console.log("you clicked me")}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<ArrowForwardIosIcon></ArrowForwardIosIcon>}
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={handleTitle}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          value={title}
+          sx={useStyles.field}
+          error={isError}
+        ></TextField>
+        <TextField
+          sx={useStyles.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          value={desc}
+          onChange={handleDesc}
+          fullWidth
+          required
+          error={isError}
+        ></TextField>
+
+        <RadioGroup>
+          <FormControlLabel
+            control={<Radio></Radio>}
+            label="Money"
+            value="value"
+            sx={useStyles.radio}
+          ></FormControlLabel>
+          <FormControlLabel
+            control={<Radio></Radio>}
+            label="crypto"
+            value="crypto"
+          ></FormControlLabel>
+        </RadioGroup>
+
+        <Button
+          // sx={useStyles.btn}
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<ArrowForwardIosIcon></ArrowForwardIosIcon>}
+        >
+          Submit
+        </Button>
+      </form>
+
+      {/* <TextField></TextField> */}
       {/* <AcUnitIcon color="secondary" fontSize="large"></AcUnitIcon>
       <AcUnitIcon color="action" fontSize="small"></AcUnitIcon>
       <AcUnitIcon color="error" fontSize="small"></AcUnitIcon>
